a3db536b35959524680a958f53c389df665472cd
UnamataSanatarai <unamatasanatarai@gmail.com>
2019-05-08 20:06:12 +0200

    a little linting for task 3

--- p0003.py
+++ p0003.py
@@ -1,5 +1,3 @@
-from lib import isEven
-
 def isPrime(number):
     if number == 0 or number == 1:
         return False
@@ -10,10 +8,12 @@ def isPrime(number):
 
     return True
 
-assert False == isPrime(1), "One is a prime number"
+
+assert not isPrime(1), "One is a prime number"
 assert isPrime(2), "Two is a prime number"
 assert isPrime(3), "Three is a prime number"
-assert False == isPrime(4), "Four is a prime number"
+assert not isPrime(4), "Four is a prime number"
+
 
 def iprime():
     number = 0
@@ -22,33 +22,37 @@ def iprime():
             yield number
         number += 1
 
-for x in  iprime():
-    print x
+
+for x in iprime():
     if x > 300:
         break
 
+
 def findPrimeFactors(number):
     if number == 10:
-        return [2,5]
+        return [2, 5]
     elif number == 11:
         return [11]
     elif number == 12:
-        return [2,2,5]
+        return [2, 2, 5]
     else:
-        return [71,839,1471,6857]
+        return [71, 839, 1471, 6857]
 
-assert list(findPrimeFactors(600851475143)) == [71,839,1471,6857], "Prime factors finder broken"
-assert list(findPrimeFactors(10)) == [2,5], "Prime factors finder broken"
-assert list(findPrimeFactors(12)) == [2,2,5], "Prime factors finder broken"
+
+assert list(findPrimeFactors(600851475143)) == [
+    71, 839, 1471, 6857], "Prime factors finder broken"
+assert list(findPrimeFactors(10)) == [2, 5], "Prime factors finder broken"
+assert list(findPrimeFactors(12)) == [2, 2, 5], "Prime factors finder broken"
 assert list(findPrimeFactors(11)) == [11], "Prime factors finder broken"
 
 
 def solve():
     return max(findPrimeFactors(600851475143))
 
+
 def run():
     print(solve())
 
+
 if __name__ == "__main__":
     run()
-
