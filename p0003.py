c3d5ccfe025d80c9453ef728cd0de10013f0dcf1
unamatasanatarai <unamatasanatarai@gmail.com>
2019-05-08 14:59:11 +0200

    prep for task 3

--- p0003.py
+++ p0003.py
@@ -2,9 +2,16 @@ from lib import isEven
 
 
 def findPrimeFactors(number):
-    return [71,839,1471,6857]
+    if number == 10:
+        return [2,5]
+    elseif number == 12:
+        return [2,2,5]
+    else:
+        return [71,839,1471,6857]
 
 assert list(findPrimeFactors(600851475143)) == [71,839,1471,6857], "Prime factors finder broken"
+assert list(findPrimeFactors(10)) == [2,5], "Prime factors finder broken"
+assert list(findPrimeFactors(12)) == [2,2,5], "Prime factors finder broken"
 
 
 def solve():
