a81a5f6830b614aa19f7f7dd6c3ecef1f94e28cb
unamatasanatarai <unamatasanatarai@gmail.com>
2019-05-09 14:01:44 +0200

    third task

--- p0003.py
+++ p0003.py
@@ -1,4 +1,5 @@
 from lib import is_prime
+import math
 
 def iprime():
     yield 2
@@ -10,27 +11,16 @@ def iprime():
 
 
 def findPrimeFactors(number):
-    if number <= 3:
-        return [3]
+    factors = []
+    divisor = 2
 
-    max = number >> 1
-    
-    if number == 10:
-        return [2, 5]
-    elif number == 11:
-        return [11]
-    elif number == 12:
-        return [2, 2, 5]
-    else:
-        return [71, 839, 1471, 6857]
-
-
-assert list(findPrimeFactors(3)) == [3], "Cannot find factors for 3"
-assert list(findPrimeFactors(10)) == [2, 5], "Cannot find factors for 10"
-assert list(findPrimeFactors(11)) == [11], "Cannot find factors for 11"
-assert list(findPrimeFactors(12)) == [2, 2, 5], "Cannot find factors for 12"
-assert list(findPrimeFactors(600851475143)) == [
-    71, 839, 1471, 6857], "Cannot find factors for 600851475143"
+    while(number > 2):
+        if (number % divisor == 0):
+            factors.append(divisor)
+            number = number / divisor
+        else:
+            divisor += 1
+    return factors
 
 
 def solve():
