88461207f0319e0cc502b7d718ebabb4d39ae2b3
UnamataSanatarai <unamatasanatarai@gmail.com>
2019-05-10 21:19:40 +0200

    fixed fibonacci generator

--- p0002.py
+++ p0002.py
@@ -4,14 +4,16 @@ from lib import isEven
 def ifibonacci(max):
     now = 1
     next = 1
-    while (next < max):
-        yield next
-        next, now = now+next, next
+    while True:
+        if now > max:
+            break
+        yield now
+        next, now = now + next, next
 
-assert list(ifibonacci(10)) == [1,2,3,5,8], "Fibonacci broken"
+assert list(ifibonacci(10)) == [1, 1, 2, 3, 5, 8], "Fibonacci broken"
 
 def solve():
-    max = 4000000
+    max = 4_000_000
     fib = sum(x for x in ifibonacci(max) if isEven(x))
 
     return fib
