570ef7b192789f0aa886c9ef0e063a027f5e771c
UnamataSanatarai <unamatasanatarai@gmail.com>
2019-05-10 18:37:17 +0200

    task 6 complete

---
+++ p0006.py
@@ -0,0 +1,15 @@
+from math import pow
+
+def calculate_difference_for(numbers):
+    sum_of_powers = sum([x * x for x in range(numbers + 1)])
+    power_of_sum = pow((numbers * (numbers + 1)) / 2, 2)
+    return power_of_sum - sum_of_powers
+
+def solve():
+    return int(calculate_difference_for(100))
+
+def run():
+    print(solve())
+
+if __name__ == "__main__":
+    run()
