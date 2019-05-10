961f369ab28c0027d740f12728c4660876f91896
UnamataSanatarai <unamatasanatarai@gmail.com>
2019-05-10 21:01:26 +0200

    task 20

---
+++ p0020.py
@@ -0,0 +1,16 @@
+from functools import partial, reduce
+from lib import compose
+
+def sum_factorial_digits(num):
+    r = reduce(lambda c, t: c*t, range(1, num+1), 1)
+    f = compose(partial(map, int), list, str)
+    return reduce(lambda c, t: c + t, f(r), 0)
+
+def solve():
+    return sum_factorial_digits(100)
+
+def run():
+    print(solve())
+
+if __name__ == "__main__":
+    run()
