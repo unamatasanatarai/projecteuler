2256bc052649d37630fd76f21e23da9deef446e6
UnamataSanatarai <unamatasanatarai@gmail.com>
2019-05-18 13:02:24 +0200

    wip

--- run.py
+++ run.py
@@ -71,31 +71,6 @@ TASKS = [
     "036_double_base_palindromes",
     "037_truncatable_primes",
 ]
-TESTS = {
-    "001_multiples_of_3_and_5": 233168,
-    "002_even_fibonacci_numbers": 4613732,
-    "003_largest_prime_factor": 6857,
-    "004_largest_palindrome_product": 906609,
-    "005_smallest_multiple": 232792560,
-    "006_sum_square_difference": 25164150,
-    "007_10001st_prime": 104743,
-    "008_largest_product_in_a_series": 23514624000,
-    "009_special_pythagorean_triplet": 31875000,
-    "010_summation_of_primes": 142913828922,
-    "011_largest_product_in_a_grid": 70600674,
-    "012_highly_divisible_triangular_number": 76576500,
-    "013_large_sum": 5537376230,
-    "014_longest_collatz_sequence": 837799,
-    "015_lattice_paths": 137846528820,
-    "016_power_digit_sum": 1366,
-    "017_number_letter_counts": 1366,
-    "020_factorial_digit_sum": 648,
-    "028_number_spiral_diagonals": 669171001,
-    "030_digit_fifth_powers": 443839,
-    "034_digital_factorials": 40730,
-    "036_double_base_palindromes": 872187,
-    "037_truncatable_primes": 748317,
-}
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
